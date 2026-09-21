7c9bb70e2a41ec3de415123972d589f8825af3c1
RajaaSouini <rajaasouini@gmail.com>
2026-09-21 11:47:27 +0000

    ex19

---
+++ ex18.js
@@ -0,0 +1,13 @@
+/*
+function calc(a , b){
+    let result = a + b;
+    return result;
+}
+console.log(result);
+*/
+//dans le premier code result is undefined 
+function calc(a , b){
+    let result = a + b;
+    return result;
+}
+console.log(calc(2,4));
---
+++ ex19.js
@@ -0,0 +1,9 @@
+for(let i = 0 ; i < 5 ; i++){
+
+}
+console.log(i);
+//cela affiche que i is not defined parceque le i est une variable fait
+//dans la boucle pour parcourir cette derniere donc elle est local dans la boucle 
+
+//et on peut à l exterieur definir une variable i du meme nom ça ne fera pas un probleme
+//car ce n ai pas la meme variable 
