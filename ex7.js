5502d16d90c0ebe304fd1103deaf73f90f0cea5d
RajaaSouini <rajaasouini@gmail.com>
2026-09-21 11:07:02 +0000

    ex7

---
+++ ex7.js
@@ -0,0 +1,9 @@
+function square(n){
+    let result = n**2;
+    return result;
+}
+function sumOfSquare(a, b){
+    let result2 = square(a) + square(b);
+    return result2;
+}
+console.log(sumOfSquare(2,3));
