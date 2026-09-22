347cf71655f762ccfa41d790e0b21f5a3d564bfe
RajaaSouini <rajaasouini@gmail.com>
2026-09-22 11:57:59 +0000

    functions

---
+++ functionsOnArraysJS.js
@@ -0,0 +1,22 @@
+let table = [1 , 2 , 3 , 4 , 5];
+//push permet d'ajouter un element à la fin du tableau
+table.push(6);
+console.log(table);
+
+//shift and unshift 
+// elle permet de supprimer le premier elem
+table.shift();
+console.log(table);
+
+
+//unshift pour ajouter un element au premier du tableau
+
+table.unshift(0);
+console.log(table);
+
+//pop supprime le dernier elem
+table.pop();
+console.log(table);
+
+
+
